12a00ff0230cf933378a57b18aaff2cdbc252c47
1190202328 <1259029328@qq.com>
2022-06-14 21:18:16 +0800

    v0.4 画图成功一半

--- libs/utils/visualization.py
+++ libs/utils/visualization.py
@@ -2,6 +2,9 @@ import os
 import sys
 
 # add python path of PadleDetection to sys.path
+from matplotlib.ticker import MultipleLocator
+from mpl_toolkits import axisartist
+
 parent_path = os.path.abspath(os.path.join(__file__, *(['..'] * 3)))
 sys.path.insert(0, parent_path)
 from libs.utils import postprocessing
@@ -9,7 +12,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 to_visualize_name = {
-    'video_test_0000004', 'video_test_0000006', 'video_test_0000007'
+    'video_test_0000004': [],
+    'video_test_0000006': [19, 18.8, 57.3],
+    'video_test_0000007': [12, 275.60, 278.80]
 }
 
 
@@ -20,54 +25,69 @@ def get_data():
     # array -> dict
     results = postprocessing.results_to_array(results, num_pred=num_pred)
 
-    processed_results = {
-        'video-id': [],
-        't-start': [],
-        't-end': [],
-        'label': [],
-        'score': []
-    }
-    print(results['video_test_0000004'])
-    draw(results['video_test_0000004'])
-    # for vid in to_visualize_name:
-    #     result = results[vid]
-    #     pred_score, pred_segment, pred_label = \
-    #         result['score'], result['segment'], result['label']
-    #     num_segs = min(num_pred, len(pred_score))
-    #
-    #     processed_results['video-id'].extend([vid] * num_segs * topk)
-    #     processed_results['t-start'].append(pred_segment[:, 0])
-    #     processed_results['t-end'].append(pred_segment[:, 1])
-    #     processed_results['label'].append(pred_label)
-    #     processed_results['score'].append(pred_score)
-    # print(processed_results)
+    vid = 'video_test_0000006'
+
+    print(results[vid]['segment'][:5])
+    print(results[vid]['label'][:5])
+    print(results[vid]['score'][:5])
+    draw(results[vid], to_visualize_name[vid])
 
 
-def draw(result):
+def draw(result, info):
     # 绘制直方图
-    # figure = plt.figure()
-
-    x = np.linspace(0, 50, 5000)
-
-    y = []
-    for i in range(result['segment']):
-        start, end = result['segment'][i]
+    bound = (info[2] - info[1]) / 3
+    start = info[1] - bound
+    end = info[2] + bound
+    total_num = 1000
+    label_id = info[0]
+    #
+    gap = total_num / (end - start)
+    x = np.linspace(start, end, total_num)
+    y_onset = np.zeros(shape=(total_num,))
+    y_offset = np.zeros(shape=(total_num,))
+    for i in range(len(result['segment'])):
+        onset, offset = result['segment'][i]
         score = result['score'][i]
-        print(start, end)
-        start = round(start, 2)
-        end = round(end, 2)
-        if 20 <= start <= 22 or 22 <= end <= 24:
-            y.append(score)
-
-    plt.bar(x, y, color='blue')
+        label = int(result['label'][i])
+        if start <= onset <= end and label == label_id:
+            y_onset[int((onset - start) * gap)] += score
+        if start <= offset <= end and label == label_id:
+            y_offset[int((offset - start) * gap)] += score
+    max_y = max(max(y_onset), max(y_offset)) * 1.1
+    fig = plt.figure(figsize=(20, 4))
+    ax = axisartist.Subplot(fig, 111)
+    # 将绘图区对象添加到画布中
+    fig.add_axes(ax)
+    x_set = MultipleLocator((end - start) / total_num * 50)  # x轴的刻度
+    # 设置刻度间隔
+    ax.xaxis.set_major_locator(x_set)
+    gt_x_onset = np.ones(shape=(10,)) * info[1]
+    gt_x_offset = np.ones(shape=(10,)) * info[2]
+    gt_y = np.linspace(0, max_y, 10)
+    plt.plot(gt_x_onset, gt_y, c='black', linestyle='--')
+    plt.plot(gt_x_offset, gt_y, c='black', linestyle='--')
+    plt.bar(x, y_onset, color='steelblue', label='Predict Onset')
+    plt.bar(x, y_offset, color='coral', label='Predict Offset')
     # # 设置图表参数
-    plt.xlabel('total_score', fontsize=15, color='black')
-    plt.ylabel('interface_delta_B', fontsize=15, color='green')
-    # plt.title('score', fontsize = 20) #设置标题
-    # plt.axis([-1, 6, -2, 2])#可手动设置x轴y轴范围
-    # plt.grid(True) #设置网格
+    ax.axis["bottom"].set_axisline_style("->", size=1.5)
+    ax.axis["left"].set_axisline_style("->", size=1.5)
+    # 通过set_visible方法设置绘图区的顶部及右侧坐标轴隐藏
+    ax.axis["top"].set_visible(False)
+    ax.axis["right"].set_visible(False)
+    plt.xticks([])
+    plt.yticks([])
+    plt.legend(loc='upper right')
+    # plt.xlabel('timestamp', fontsize=15, color='black')
+    # plt.ylabel('score', fontsize=15, color='black')
 
-    plt.show()
+    output_dir = '../../ckpt/thumos_i3d_reproduce/figures'
+    if os.path.exists(output_dir):
+        for name in os.listdir(output_dir):
+            os.remove(f'{output_dir}/{name}')
+        os.removedirs(output_dir)
+    os.makedirs(output_dir)
+    output_file = '../../ckpt/thumos_i3d_reproduce/figures/test7.jpg'
+    plt.savefig(output_file)
 
 
 get_data()
