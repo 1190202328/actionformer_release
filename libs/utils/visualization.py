5ce9f58856ec871296c04dc41e5443d8ba2b2d29
1190202328 <1259029328@qq.com>
2022-06-15 15:39:52 +0800

    v1.0 画图程序完成

--- libs/utils/visualization.py
+++ libs/utils/visualization.py
@@ -10,18 +10,20 @@ sys.path.insert(0, parent_path)
 from libs.utils import postprocessing
 import matplotlib.pyplot as plt
 import numpy as np
+import imageio
+from skimage.transform import resize
 
 to_visualize_name = {
-    'video_test_0000006': [19, 18.8, 57.3, 'VolleyballSpiking'],
-    'video_test_0000011': [15, 0.4, 12.4, 'Shotput'],
-    'video_test_0000045': [18, 1.6, 8.7, 'ThrowDiscus']
+    'video_test_0000006': [19, 18.8, 57.3, 'VolleyballSpiking', 564, 1719],  # 类别id，开始秒数，结束秒数，类别名称，开始帧，结束帧
+    'video_test_0000011': [15, 0.4, 12.4, 'Shotput', 12, 372],
+    'video_test_0000058': [3, 1.9, 26.6, 'ThrowDiscus', 57, 798]
 }
 
 
 def get_data():
     num_pred = 200
-    topk = 2
     output_dir = '../../ckpt/thumos_i3d_reproduce/figures'
+    video_dir = '../../data/thumos/visualization'
     if os.path.exists(output_dir):
         for name in os.listdir(output_dir):
             os.remove(f'{output_dir}/{name}')
@@ -33,135 +35,83 @@ def get_data():
     results = postprocessing.results_to_array(results, num_pred=num_pred)
 
     for vid in to_visualize_name:
-        # print(results[vid]['segment'][:5])
-        # print(results[vid]['label'][:5])
-        # print(results[vid]['score'][:5])
-        # print(results[vid]['point'][:5])
-        draw_regression_classification(results[vid], to_visualize_name[vid],
-                                       f'../../ckpt/thumos_i3d_reproduce/figures/{vid}.jpg')
+        draw(results[vid], to_visualize_name[vid], f'{video_dir}/{vid}.mp4',
+             f'../../ckpt/thumos_i3d_reproduce/figures/{vid}.jpg')
         print(f'{vid}.jpg generated!')
 
 
-# def draw_regression_classification(result, info, output_file):
-#     # 绘制直方图
-#     bound = (info[2] - info[1]) / 3
-#     start = info[1] - bound
-#     end = info[2] + bound
-#     label_id = info[0]
-#     class_name = info[3]
-#     #
-#     total_num = 1000
-#     gap = total_num / (end - start)
-#     #
-#     x = np.linspace(start, end, total_num)
-#     y_onset = np.zeros(shape=(total_num,))
-#     y_offset = np.zeros(shape=(total_num,))
-#     for i in range(len(result['segment'])):
-#         onset, offset = result['segment'][i]
-#         score = result['score'][i]
-#         label = int(result['label'][i])
-#         if start <= onset <= end and label == label_id:
-#             y_onset[int((onset - start) * gap)] += score
-#         if start <= offset <= end and label == label_id:
-#             y_offset[int((offset - start) * gap)] += score
-#     y_point = np.zeros(shape=(total_num,))
-#     for i in range(len(result['point'])):
-#         point, label, score = result['point'][i]
-#         if start <= point <= end and label == label_id:
-#             y_point[int((point - start) * gap)] += score
-#     max_y = max(max(y_onset), max(y_offset)) * 1.1
-#     fig = plt.figure(figsize=(20, 8))
-#
-#     ax = axisartist.Subplot(fig, 211)
-#     # 将绘图区对象添加到画布中
-#     fig.add_axes(ax)
-#     x_set = MultipleLocator((end - start) / total_num * 50)  # x轴的刻度
-#     # 设置刻度间隔
-#     ax.xaxis.set_major_locator(x_set)
-#     gt_x_onset = np.ones(shape=(10,)) * info[1]
-#     gt_x_offset = np.ones(shape=(10,)) * info[2]
-#     gt_y = np.linspace(0, max_y, 10)
-#     ax.plot(gt_x_onset, gt_y, c='black', linestyle='--')
-#     ax.plot(gt_x_offset, gt_y, c='black', linestyle='--')
-#     ax.bar(x, y_point, color='red', label=f'Classification Scores for "{class_name}"')
-#     # # 设置图表参数
-#     ax.axis["bottom"].set_axisline_style("->", size=1.5)
-#     ax.axis["left"].set_axisline_style("->", size=1.5)
-#     # 通过set_visible方法设置绘图区的顶部及右侧坐标轴隐藏
-#     ax.axis["top"].set_visible(False)
-#     ax.axis["right"].set_visible(False)
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.legend(loc='upper right')
-#
-#     ax = axisartist.Subplot(fig, 212)
-#     # 将绘图区对象添加到画布中
-#     fig.add_axes(ax)
-#     x_set = MultipleLocator((end - start) / total_num * 50)  # x轴的刻度
-#     # 设置刻度间隔
-#     ax.xaxis.set_major_locator(x_set)
-#     gt_x_onset = np.ones(shape=(10,)) * info[1]
-#     gt_x_offset = np.ones(shape=(10,)) * info[2]
-#     gt_y = np.linspace(0, max_y, 10)
-#     ax.plot(gt_x_onset, gt_y, c='black', linestyle='--')
-#     ax.plot(gt_x_offset, gt_y, c='black', linestyle='--')
-#     ax.bar(x, y_onset, color='steelblue', label='Predict Onset')
-#     ax.bar(x, y_offset, color='coral', label='Predict Offset')
-#     # # 设置图表参数
-#     ax.axis["bottom"].set_axisline_style("->", size=1.5)
-#     ax.axis["left"].set_axisline_style("->", size=1.5)
-#     # 通过set_visible方法设置绘图区的顶部及右侧坐标轴隐藏
-#     ax.axis["top"].set_visible(False)
-#     ax.axis["right"].set_visible(False)
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.legend(loc='upper right')
-#
-#     plt.savefig(output_file)
+def draw(result, info, video_path, output_file):
+    # 绘图
+    total_num = 1000
+    end = 100
 
-def draw_regression_classification(result, info, output_file):
-    # 绘制直方图
-    # TODO 需要归一化图像
-    factor = 50 / (info[2] - info[1])
     bound = (info[2] - info[1]) / 3
-    start = info[1] - bound
-    end = info[2] + bound
-    start *= factor
-    end *= factor
-
+    factor = total_num / ((info[2] - info[1]) + 2 * bound)
+    left = info[1] - bound
+    right = info[2] + bound
     label_id = info[0]
     class_name = info[3]
     #
-    total_num = 1000
-    gap = total_num / (end - start)
-    #
-    x = np.linspace(start, end, total_num)
-    y_onset = np.zeros(shape=(total_num,))
-    y_offset = np.zeros(shape=(total_num,))
+    x = np.linspace(0, end, total_num)
+    y_onset = np.zeros(shape=x.shape)
+    y_offset = np.zeros(shape=x.shape)
     for i in range(len(result['segment'])):
         onset, offset = result['segment'][i]
         score = result['score'][i]
         label = int(result['label'][i])
-        if start <= onset <= end and label == label_id:
-            y_onset[int((onset - start) * gap)] += score
-        if start <= offset <= end and label == label_id:
-            y_offset[int((offset - start) * gap)] += score
-    y_point = np.zeros(shape=(total_num,))
+        if left <= onset <= right and label == label_id:
+            y_onset[int((onset - left) * factor)] += score
+        if left <= offset <= right and label == label_id:
+            y_offset[int((offset - left) * factor)] += score
+    y_point = np.zeros(shape=x.shape)
     for i in range(len(result['point'])):
         point, label, score = result['point'][i]
-        if start <= point <= end and label == label_id:
-            y_point[int((point - start) * gap)] += score
-    max_y = max(max(y_onset), max(y_offset)) * 1.1
-    fig = plt.figure(figsize=(20, 8))
+        if left <= point <= right and label == label_id:
+            y_point[int((point - left) * factor)] += score
 
-    ax = axisartist.Subplot(fig, 211)
+    # 1.画最上面的图片
+    fig = plt.figure(figsize=(20, 12))
+    
+    ax = axisartist.Subplot(fig, 311)
+    fig.add_axes(ax)
+    try:
+        video = imageio.get_reader(video_path, 'ffmpeg')
+        onset_frame = None
+        mid_frame = None
+        offset_frame = None
+        height, width = 180, 320
+        for num, im in enumerate(video):
+            if num == info[4]:
+                onset_frame = np.asarray(resize(im, (height, width)))
+            if num == int((info[4] + info[5]) / 2):
+                mid_frame = np.asarray(resize(im, (height, width)))
+            if num == info[5]:
+                offset_frame = np.asarray(resize(im, (height, width)))
+        side_margin = np.ones(shape=(height, int(width * 0.5), 3))
+        mid_margin = np.ones(shape=(height, int(width * 0.2), 3))
+        frame = np.concatenate([side_margin, onset_frame, mid_margin, mid_frame, mid_margin, offset_frame, side_margin],
+                               axis=1)
+
+        ax.axis["bottom"].set_visible(False)
+        ax.axis["left"].set_visible(False)
+        ax.axis["top"].set_visible(False)
+        ax.axis["right"].set_visible(False)
+        plt.xticks([])
+        plt.yticks([])
+        ax.imshow(frame)
+    except FileNotFoundError:
+        pass
+
+    # 2.画中间的classification
+    ax = axisartist.Subplot(fig, 312)
     # 将绘图区对象添加到画布中
     fig.add_axes(ax)
-    x_set = MultipleLocator((end - start) / total_num * 50)  # x轴的刻度
+    x_set = MultipleLocator(end / total_num * 50)  # x轴的刻度
     # 设置刻度间隔
     ax.xaxis.set_major_locator(x_set)
-    gt_x_onset = np.ones(shape=(10,)) * info[1]
-    gt_x_offset = np.ones(shape=(10,)) * info[2]
+    gt_x_onset = np.ones(shape=(10,)) * (info[1] - left) / ((info[2] - info[1]) + 2 * bound) * end
+    gt_x_offset = np.ones(shape=(10,)) * (info[2] - left) / ((info[2] - info[1]) + 2 * bound) * end
+    max_y = max(y_point) * 1.1
     gt_y = np.linspace(0, max_y, 10)
     ax.plot(gt_x_onset, gt_y, c='black', linestyle='--')
     ax.plot(gt_x_offset, gt_y, c='black', linestyle='--')
@@ -176,14 +126,16 @@ def draw_regression_classification(result, info, output_file):
     plt.yticks([])
     plt.legend(loc='upper right')
 
-    ax = axisartist.Subplot(fig, 212)
+    # 3.画下面的regression
+    ax = axisartist.Subplot(fig, 313)
     # 将绘图区对象添加到画布中
     fig.add_axes(ax)
-    x_set = MultipleLocator((end - start) / total_num * 50)  # x轴的刻度
+    x_set = MultipleLocator(end / total_num * 50)  # x轴的刻度
     # 设置刻度间隔
     ax.xaxis.set_major_locator(x_set)
-    gt_x_onset = np.ones(shape=(10,)) * info[1]
-    gt_x_offset = np.ones(shape=(10,)) * info[2]
+    gt_x_onset = np.ones(shape=(10,)) * (info[1] - left) / ((info[2] - info[1]) + 2 * bound) * end
+    gt_x_offset = np.ones(shape=(10,)) * (info[2] - left) / ((info[2] - info[1]) + 2 * bound) * end
+    max_y = max(max(y_onset), max(y_offset)) * 1.1
     gt_y = np.linspace(0, max_y, 10)
     ax.plot(gt_x_onset, gt_y, c='black', linestyle='--')
     ax.plot(gt_x_offset, gt_y, c='black', linestyle='--')
