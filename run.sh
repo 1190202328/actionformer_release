22e2325afbfe9c0b4908a3175a98ad908a83cb0d
1190202328 <1259029328@qq.com>
2022-09-01 17:51:55 +0800

    v2.2.0 开始ActivityNet的消融实验

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-# THUMOS14 with I3D features
-python ./train.py ./configs/thumos_i3d.yaml --output reproduce
-python ./eval.py ./configs/thumos_i3d.yaml ./ckpt/thumos_i3d_reproduce
+## THUMOS14 with I3D features
+#python ./train.py ./configs/thumos_i3d.yaml --output reproduce
+#python ./eval.py ./configs/thumos_i3d.yaml ./ckpt/thumos_i3d_reproduce
+
+python ./train.py ./configs/anet_tsp.yaml --output reproduce
+python ./eval.py ./configs/anet_tsp.yaml ./ckpt/anet_tsp_reproduce
